55e870d23ace814000b980206e7aa80fd7367092
mvrthn <martin@agb.home.pl>
2023-04-13 00:23:17 +0200

    main: creating enigma on stack

--- main.cpp
+++ main.cpp
@@ -3,9 +3,8 @@
 int main() {
     int r[3] = {0, 1, 2};
     int code[3] = {0, 0, 0};
-    Enigma *enigma = new Enigma(r, 0);
-    enigma->setCode(code);
-    enigma->listen();
-    delete enigma;
+    Enigma enigma(r, 0);
+    enigma.setCode(code);
+    enigma.listen();
     return 0;
 }
